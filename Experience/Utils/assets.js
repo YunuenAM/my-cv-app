6c23be67d44b10ddbae716156ab6de668b19c0e6
Yunuen AM <yunameza@outlook.com>
2023-08-25 17:46:13 -0600

    ✨Feat:   public  folder added

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,14 @@
+export  default [
+    {
+        name: 'room',
+        type: 'objModel',
+        path: '/models/deskcv.obj'
+    },
+
+    {
+        name: 'screen',
+        type: 'videoTexture',
+        path: '/textures/frog.mp4'
+
+    }
+]
