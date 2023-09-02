de5390247a6ea1a78967da74821687cbc694b5c1
Yunuen AM <yunameza@outlook.com>
2023-09-02 13:32:34 -0600

    ✨Feat:  bootstrap  installed

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import "./style.css"
 import Experience from "./Experience/Experience.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
