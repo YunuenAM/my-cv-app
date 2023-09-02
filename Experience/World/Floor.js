5c31f72aae4ebdbba3a89b552ffae27755fd938a
Yunuen AM <yunameza@outlook.com>
2023-09-02 10:35:21 -0600

    ✨Feat:   floor position updated

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -25,6 +25,7 @@ export default class Floor {
     this.plane = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.plane);
     this.plane.rotation.x = -Math.PI/2;
+    this.plane.position.y = -0.5;
 
     }
 
