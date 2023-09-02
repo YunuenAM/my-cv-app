6d6b926e1ac898616a0b87e125523651efa16d71
Yunuen AM <yunameza@outlook.com>
2023-09-02 09:17:13 -0600

    ✨Feat:  Controls.js added

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -26,6 +26,36 @@ export default class Camera {
         this.scene.add(this.perspectiveCamera);
     }
 
+
+    createOrthographicCamera() {
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum) / 2,
+            (this.sizes.aspect * this.sizes.frustrum) / 2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -50,
+            50
+        );
+
+        // 6.5
+        this.orthographicCamera.position.y = 5.65;
+        this.orthographicCamera.position.z = 10;
+        this.orthographicCamera.rotation.x = -Math.PI / 6;
+
+        this.scene.add(this.orthographicCamera);
+
+        // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+        // this.scene.add(this.helper);
+
+        const size = 20;
+        const divisions = 20;
+
+        // const gridHelper = new THREE.GridHelper(size, divisions);
+        // this.scene.add(gridHelper);
+
+        // const axesHelper = new THREE.AxesHelper(10);
+        // this.scene.add(axesHelper);
+    }
     setOrbitControls() {
         this.controls = new OrbitControls(this.perspectiveCamera, this.canvas);
         this.controls.enableDamping = true;
--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -7,6 +7,12 @@ export default class Sizes extends EventEmitter{
     this.height = window.innerHeight;
     this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio,2);
+    this.frustrum = 5;
+    if (this.width < 968) {
+        this.device = "mobile";
+    } else {
+        this.device = "desktop";
+    }
 
     window.addEventListener('resize',()=>{
 
@@ -16,6 +22,13 @@ export default class Sizes extends EventEmitter{
         this.pixelRatio =  Math.min(window.devicePixelRatio,2);
         this.emit('resize');
 
+        if (this.width < 968 && this.device !== "mobile") {
+          this.device = "mobile";
+          this.emit("switchdevice", this.device);
+      } else if (this.width >= 968 && this.device !== "desktop") {
+          this.device = "desktop";
+          this.emit("switchdevice", this.device);
+      }
     })
   }
 }
