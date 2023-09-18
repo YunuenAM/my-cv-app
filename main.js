223d8cf45a5a69df440e899d514bceb47529ac8a
Yunuen AM <yunameza@outlook.com>
2023-09-18 15:29:38 -0600

    ✨SweetALert added

--- main.js
+++ main.js
@@ -2,6 +2,49 @@ import "./style.css"
 import Experience from "./Experience/Experience.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import swal from "sweetalert2";
 
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+
+    try {
+        const response = await fetch(e.target.action, {
+            method: e.target.method,
+            body: form,
+            headers: {
+                Accept: 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            e.target.reset();
+            Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: 'Thanks, I will contact you soon'
+            });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops!',
+                text: 'Something went wrong'
+            });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops!',
+            text: 'Something went wrong'
+        });
+    }
+};
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
+
+const form =  document.querySelector("#form")
+
+form.addEventListener("submit", handleSubmit);
+
